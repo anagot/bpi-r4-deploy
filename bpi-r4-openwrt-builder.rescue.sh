ab88a2da2e8e853f9bb6d6b54bba8f168905b5eb
Petr Wozniak <petr.wozniak@gmail.com>
2026-03-26 15:43:36 +0100

    full emmc rel.

--- bpi-r4-openwrt-builder.rescue.sh
+++ bpi-r4-openwrt-builder.rescue.sh
@@ -9,33 +9,52 @@ cd openwrt; git checkout f505120278fdb752586853f4df7482150d0add3b; cd -;		#ipq40
 git clone --branch master https://git01.mediatek.com/openwrt/feeds/mtk-openwrt-feeds
 cd mtk-openwrt-feeds; git checkout 07ef2962013b19a4a1e9f8c34a21c1e90be691ce; cd -;	#[MAC80211][WiFi6/7/8][app][Fix iwpriv/ated script]
 
-cd openwrt
-bash ../mtk-openwrt-feeds/autobuild/unified/autobuild.sh filogic prepare 
+\cp -r my_files/999-sfp-10-additional-quirks.patch mtk-openwrt-feeds/25.12/files/target/linux/mediatek/patches-6.12
 
-scripts/feeds uninstall crypto-eip pce tops-tool
+### tx_power check Ivan Mironov's patch - for defective BE14 boards with defective eeprom flash
+\cp -r my_files/100-wifi-mt76-mt7996-Use-tx_power-from-default-fw-if-EEP.patch mtk-openwrt-feeds/autobuild/unified/filogic/mac80211/25.12/files/package/kernel/mt76/patches
+
+cd openwrt
+bash ../mtk-openwrt-feeds/autobuild/unified/autobuild.sh filogic-mac80211-mt798x_rfb-wifi7_nic prepare
 
 \cp -r ../my_files/450-w-nand-mmc-add-bpi-r4.patch package/boot/uboot-mediatek/patches/450-add-bpi-r4.patch
 \cp -r ../my_files/w-nand-mmc-filogic.mk target/linux/mediatek/image/filogic.mk
 
-mkdir -p files/root/bpi-r4-install
-\cp ../my_files/bpi-r4-install/snand-img.bin files/root/bpi-r4-install/
-\cp ../my_files/bpi-r4-install/install-nand.sh files/root/bpi-r4-install/
-#\cp ../my_files/bpi-r4-install/install-emmc.sh files/root/bpi-r4-install/
-chmod +x files/root/bpi-r4-install/install-nand.sh
+\cp -r ../my_files/sms-tool/ feeds/packages/utils/sms-tool
+\cp -r ../my_files/modemdata-main/ feeds/packages/utils/modemdata 
+\cp -r ../my_files/luci-app-modemdata-main/luci-app-modemdata/ feeds/luci/applications
+\cp -r ../my_files/luci-app-lite-watchdog/ feeds/luci/applications
+\cp -r ../my_files/luci-app-sms-tool-js-main/luci-app-sms-tool-js/ feeds/luci/applications
+
+./scripts/feeds update -a
+./scripts/feeds install -a
+
+\cp -r ../my_files/qmi.sh package/network/utils/uqmi/files/lib/netifd/proto/
+chmod -R 755 package/network/utils/uqmi/files/lib/netifd/proto
+chmod -R 755 feeds/luci/applications/luci-app-modemdata/root
+chmod -R 755 feeds/luci/applications/luci-app-sms-tool-js/root
+chmod -R 755 feeds/packages/utils/modemdata/files/usr/share
+
+
+#mkdir -p files/root/bpi-r4-install
+#\cp ../my_files/bpi-r4-install/snand-img.bin files/root/bpi-r4-install/
+#\cp ../my_files/bpi-r4-install/install-nand.sh files/root/bpi-r4-install/
+#cp ../my_files/bpi-r4-install/install-emmc.sh files/root/bpi-r4-install/
+#chmod +x files/root/bpi-r4-install/install-nand.sh
 #chmod +x files/root/bpi-r4-install/install-emmc.sh
 
 # Set hostname for rescue system
 mkdir -p files/etc/uci-defaults
 cat > files/etc/uci-defaults/99-hostname << 'EOF'
-uci set system.@system[0].hostname='OpenWrt-SD-rescue'
+uci set system.@system[0].hostname='OpenWrt-eMMC'
 uci commit system
 EOF
 
-\cp -r ../configs/rescue.defconfig .config
+#\cp -r ../my_files/my_final_defconfig .config
+\cp -r ../configs/config.hnat.la .config
 make defconfig
 
-
-bash ../mtk-openwrt-feeds/autobuild/unified/autobuild.sh filogic build
+bash ../mtk-openwrt-feeds/autobuild/unified/autobuild.sh filogic-mac80211-mt798x_rfb-wifi7_nic build
 
 exit
 
